6e361221aaae8b88c6f2144cfb9037b4efb211c6
Lucas-ReyT <reytorrecillas.lucas@gmail.com>
2025-04-16 09:57:47 +0200

    route création

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
-from db import collection
+from flask import Flask, jsonify, request
+from db import collection, collection_users
+
 
 app = Flask(__name__)
 
@@ -43,6 +44,46 @@ def get_pokemon_by_type(type_name):
         return jsonify({'error': f'No Pokémon found with type {type_name}'}), 404
 
 
+
+
+
+# créer un User
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get('username')
+
+    if not username:
+        return jsonify({'error': 'Username is required'}), 400
+    
+    # Vérifie si le pseudo existe déjà
+    if collection_users.find_one({'username': username}):
+        return jsonify({'error': 'Username already exists'}), 400
+    
+    collection_users.insert_one({'username': username, 'team': []})
+    return jsonify({'message': f'User {username} created successfully'})
+
+#Créer une équipe 
+@app.route('/user/<username>/add', methods=['POST'])
+def add_pokemon_to_team(username):
+    data = request.json
+    pokemon_name = data.get('pokemon')
+
+    user = collection_users.find_one({'username': username})
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    if len(user['team']) >= 6:
+        return jsonify({'error': 'Team already has 6 Pokémon'}), 400
+
+    if pokemon_name in user['team']:
+        return jsonify({'error': 'Pokémon already in team'}), 400
+
+    collection_users.update_one(
+        {'username': username},
+        {'$push': {'team': pokemon_name}}
+    )
+    return jsonify({'message': f'{pokemon_name} added to {username}\'s team'})
 
 if __name__ == '__main__':
     app.run(debug=True)
