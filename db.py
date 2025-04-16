c4bf1973ccbe4625a33ea4aa434ebbc390bdef8b
Lucas-ReyT <reytorrecillas.lucas@gmail.com>
2025-04-16 10:22:55 +0200

    v2

--- db.py
+++ db.py
@@ -5,5 +5,4 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://MP-Death30:1234@cluster0.dtgdmge.mongodb.net/myDatabase?retryWrites=true&w=majority')
 db = client['Pokemon']
 collection = db['Pokemon_collection']
-doc = collection.find_one()
-print(doc)
+collection_users = db["users"]
